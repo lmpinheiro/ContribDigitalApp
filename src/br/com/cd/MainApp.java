69265d9c78482b1e11585f12e780a4d27e018194
Luan <Luan@pc-luan>
2015-06-26 14:20:30 -0300

    Project Start.

---
+++ src/br/com/cd/MainApp.java
@@ -0,0 +1,77 @@
+package br.com.cd;
+
+import java.io.IOException;
+
+import com.guigarage.flatterfx.FlatterFX;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    	this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("ContribDigitalApp");
+
+        initRootLayout();
+
+        showPersonOverview();
+    }
+
+    /**
+     * Inicializa o root layout (layout base).
+     */
+    public void initRootLayout() {
+        try {
+            // Carrega o root layout do arquivo fxml.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/ContribDigLayout.fxml"));
+            rootLayout = (BorderPane) loader.load();
+
+            // Mostra a scene (cena) contendo oroot layout.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Mostra o person overview dentro do root layout.
+     */
+    public void showPersonOverview() {
+        try {
+            // Carrega o person overview.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/nfe/PesquisarNfe.fxml"));
+            AnchorPane personOverview = (AnchorPane) loader.load();
+
+            // Define o person overview dentro do root layout.
+            rootLayout.setCenter(personOverview);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Retorna o palco principal.
+     * @return
+     */
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
